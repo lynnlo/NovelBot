07d5eed5d35eb3afcf12f6c4014f6e7bdecc5ebf
Lynn Ong <tharlynnhtet.ong@gmail.com>
2020-11-22 22:17:41 -0700

    Update trainer, trained new model.

--- src/main.py
+++ src/main.py
@@ -1,71 +1,62 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
+# To add a new cell, type ''
+# To add a new markdown cell, type ' [markdown]'
+
 # Imports
 
+import keras
 from keras import Sequential
 from keras.layers import LSTM, Dense, Dropout, Dropout, Activation, Reshape
 from keras.losses import categorical_crossentropy
-from sklearn import preprocessing
 import numpy as np
 
-# %%
 # Sort
 
-text = open("././src/data/romeoandjuliet.txt", "r").read()
+text = open("././src/data/other.txt", "r").read()
 
 chardict = sorted(list(set(text)))
 
 total = len(text)
 chars = len(chardict)
 
-print("Total Charaters        :", total)
+print("Total Charaters    :", total)
 print("Total Unique Charaters :", chars)
 
-
-# %%
 # Format
 
-chunklength = 25
+chunklength = 75
 step = 1
 sentences = []
 characters = []
 
 for i in range(0, len(text) - chunklength, step):
-    sentences.append(text[i : i + chunklength])
-    characters.append(text[i + chunklength])
+	sentences.append(text[i : i + chunklength])
+	characters.append(text[i + chunklength])
 
 chunks = len(sentences)
-print("Total Chunks :", chunks)
+print("Total Chunks       : ", chunks)
 
-
-# %%
 # Sample
 
-print("Sample Chunk     : ", sentences[0])
-print("Sample Character : ", characters[0])
+print("Sample Chunk       : ", sentences[0])
+print("Sample Character    : ", characters[0])
 
-
-# %%
 # Format
 
 x = np.zeros(chunks * chunklength * chars, np.bool).reshape(chunks, chunklength, chars)
 y = np.zeros(chunks * 1 * chars, np.bool).reshape(chunks, 1, chars)
 
 for i,v in enumerate(sentences):
-    for a,b in enumerate(v):
-        x[i][a][chardict.index(b)] = True
+	for a,b in enumerate(v):
+		x[i][a][chardict.index(b)] = True
 
 for i,v in enumerate(characters):
-    y[i][0][chardict.index(v)] = True
+	y[i][0][chardict.index(v)] = True
 
 print("Total Data Values  : ", chunks * chunklength * chars)
 print("Total Label Values : ", chunks * 1 * chars)
 print("X Shape :", x.shape)
 print("Y Shape :", y.shape)
 
-
-# %%
 # Model
 
 model = Sequential()
@@ -80,75 +71,64 @@ model.summary()
 
 model.compile(optimizer="rmsprop", loss=categorical_crossentropy)
 
+# Load
 
-# %%
+model = keras.models.load_model("././src/models/novelbot1")
+
 # Train
 
-model.fit(x=x, y=y, batch_size=chunklength, epochs=4)
+#model.fit(x=x, y=y, batch_size=chunklength, epochs=5)
 
+# Save
 
-# %%
-# User Input
+#model.save("././src/models/novelbot1")
 
-userinput = chardict[0] * 5 or sentences[0]
-
-userdata = np.zeros(chunklength * chars, np.bool).reshape(1, chunklength, chars)
-
-for i,v in enumerate(userinput):
-        userdata[0][i][chardict.index(v)] = True
-
-
-# %%
 # Prediction
 
-inputdata = x[53].reshape(1, chunklength, chars)
+inputdata = x[round(len(x)/2)].reshape(1, chunklength, chars)
 
 prediction = model.predict(inputdata)[0]
 
 print("Input shape : ", inputdata.shape)
 
-
-# %%
 # Clean
 
 totalprediction = ""
-length = 250
+length = 500
 
 for i in range(length):
 
-    cleaninput = []
-    cleanprediction = []
+	cleaninput = []
+	cleanprediction = []
 
-    for a in prediction:
-        bi, bv = 1, -1
-        for i,v in enumerate(a):
-            if v > bv:
-                bv = v
-                bi = i
-        cleanprediction.append(chardict[bi])
+	for a in prediction:
+		bi, bv = 1, -1
+		for i,v in enumerate(a):
+			if v > bv:
+				bv = v
+				bi = i
+		cleanprediction.append(chardict[bi])
 
-    for a in inputdata:
-        s = []
-        for b in a:
-            for i,v in enumerate(b):
-                if v:
-                    s.append(chardict[i])
-        cleaninput.append("".join(s))
+	for a in inputdata:
+		s = []
+		for b in a:
+			for i,v in enumerate(b):
+				if v:
+					s.append(chardict[i])
+		cleaninput.append("".join(s))
 
-    # New Prediction
+	# New Prediction
 
-    newinput = "".join(list(i for i in cleaninput[0])[1:]) + cleanprediction[0]
+	newinput = "".join(list(i for i in cleaninput[0])[1:]) + cleanprediction[0]
 
-    userdata = np.zeros(chunklength * chars, np.bool).reshape(1, chunklength, chars)
+	userdata = np.zeros(chunklength * chars, np.bool).reshape(1, chunklength, chars)
 
-    for i,v in enumerate(newinput):
-            userdata[0][i][chardict.index(v)] = True
-    inputdata = userdata
+	for i,v in enumerate(newinput):
+		userdata[0][i][chardict.index(v)] = True
+	inputdata = userdata
 
-    prediction = model.predict(inputdata)[0]
+	prediction = model.predict(inputdata)[0]
 
-    totalprediction += cleanprediction[0]
+	totalprediction += cleanprediction[0]
 
-
-# %%
 print(totalprediction)
